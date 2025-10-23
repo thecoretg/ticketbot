b4ed7cc84ec7d7b8b5b657e16beb89792e591b97
Danny Rosenthal <dsrosen6@gmail.com>
2025-10-23 08:40:58 -0500

    remove unused pre-resty function

--- internal/webex/client.go
+++ internal/webex/client.go
@@ -1,10 +1,6 @@
 package webex
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"net/http"
 
 	"resty.dev/v3"
@@ -24,47 +20,4 @@ func NewClient(token string) *Client {
 	c.SetRetryCount(3)
 
 	return &Client{restClient: c}
-}
-
-func (c *Client) request(ctx context.Context, method, endpoint string, payload io.Reader, target interface{}) error {
-	url := fmt.Sprintf("%s/%s", baseUrl, endpoint)
-	req, err := http.NewRequestWithContext(ctx, method, url, payload)
-	if err != nil {
-		return fmt.Errorf("creating the request: %w", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.apiKey))
-
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("sending the request: %w", err)
-	}
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Printf("error closing response body: %v", err)
-		}
-	}(res.Body)
-	if res.StatusCode < 200 || res.StatusCode >= 300 {
-		data, err := io.ReadAll(res.Body)
-		if err != nil {
-			return fmt.Errorf("reading error response body: %w", err)
-		}
-		return fmt.Errorf("non-success response code: %d: %s", res.StatusCode, string(data))
-	}
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("reading the response body: %w", err)
-	}
-
-	if target != nil {
-		if err := json.Unmarshal(data, target); err != nil {
-			return fmt.Errorf("unmarshaling the response to json: %w", err)
-		}
-	}
-
-	return nil
 }
